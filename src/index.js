05915366d7e7f65bd58aeef9234713841b3d59e7
crL96 <190506813+crL96@users.noreply.github.com>
2025-02-01 14:33:13 +0100

    Add function for switching tabs to the different modules using nav buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import "./styles.css";
 import {homepageLoad} from "./homepage.js";
 import {menuLoad} from "./menu.js";
+import { aboutpageLoad } from "./about.js";
 
-menuLoad();
+function clearContent() {
+    const container = document.querySelector("#content");
+    container.replaceChildren();
+}
+
+homepageLoad();
+
+const homeBtn = document.querySelector("#homeBtn");
+homeBtn.addEventListener("click", () => {
+    clearContent();
+    homepageLoad();
+});
+
+const menuBtn = document.querySelector("#menuBtn");
+menuBtn.addEventListener("click", () => {
+    clearContent();
+    menuLoad();
+});
+
+const aboutBtn = document.querySelector("#aboutBtn");
+aboutBtn.addEventListener("click", () => {
+    clearContent();
+    aboutpageLoad();
+});
